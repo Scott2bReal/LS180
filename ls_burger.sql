9d3af20c4e74f19d2e50c5aa3ec0acac32d66817
Scott Hoecker <hoeckers@gmail.com>
2022-05-10 22:02:48 -0500

    Format

--- ls_burger.sql
+++ ls_burger.sql
@@ -7,7 +7,15 @@ CREATE TABLE orders (
 );
 
 INSERT INTO orders VALUES (1, 'Todd Perez', 'LS Burger', 'Fries', 'Lemonade');
-INSERT INTO orders VALUES (2, 'Florence Jordan', 'LS Cheeseburger', 'Fries', 'Chocolate Shake');
-INSERT INTO orders VALUES (3, 'Robin Barnes', 'LS Burger', 'Onion Rings', 'Vanilla Shake');
-INSERT INTO orders VALUES (4, 'Joyce Silva', 'LS Double Deluxe Burger', 'Fries', 'Chocolate Shake');
-INSERT INTO orders VALUES (5, 'Joyce Silva', 'LS Chicken Burger', 'Onion Rings', 'Cola');
+INSERT INTO orders VALUES (
+    2, 'Florence Jordan', 'LS Cheeseburger', 'Fries', 'Chocolate Shake'
+);
+INSERT INTO orders VALUES (
+    3, 'Robin Barnes', 'LS Burger', 'Onion Rings', 'Vanilla Shake'
+);
+INSERT INTO orders VALUES (
+    4, 'Joyce Silva', 'LS Double Deluxe Burger', 'Fries', 'Chocolate Shake'
+);
+INSERT INTO orders VALUES (
+    5, 'Joyce Silva', 'LS Chicken Burger', 'Onion Rings', 'Cola'
+);
