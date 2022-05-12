1015cab7e367697f1b0486b2ad8e200d9b9996fe
Scott Hoecker <hoeckers@gmail.com>
2022-05-12 06:47:54 -0500

    Edit ls burger schema to recreate on slowboy

--- ls_burger_2.sql
+++ ls_burger_2.sql
@@ -1,8 +1,14 @@
+-- Edited to get this DB going on my other machine
+
 CREATE TABLE orders(
     id serial,
     customer_name varchar(100) NOT NULL,
+    customer_email varchar(150),
+    customer_loyalty_points int,
     burger varchar(50),
     side varchar(50),
     drink varchar(50),
-    order_total decimal(4, 2) NOT NULL
+    burger_total decimal(4, 2),
+    side_total decimal(4,2),
+    drink_total decimal(4,2)
 );
