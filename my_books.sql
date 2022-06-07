a222eb42e4df939e717890a3e7aa27c916ee8cde
Scott Hoecker <hoeckers@gmail.com>
2022-06-07 16:55:12 -0500

    Get books db up to date

---
+++ my_books.sql
@@ -0,0 +1,31 @@
+CREATE TABLE authors (
+  id serial PRIMARY KEY,
+  name text
+);
+
+CREATE TABLE books (
+  id serial PRIMARY KEY,
+  title text,
+  isbn text,
+  author_id int REFERENCES authors (id)
+);
+
+INSERT INTO authors (name)
+VALUES ('William Gibson'), ('Iain M. Banks'), ('Philip K. Dick');
+
+INSERT INTO books (title, isbn, author_id)
+VALUES (
+  'Neuromancer', '9780441569595', 1
+), (
+  'Consider Phlebas', '9780316005388', 2
+), (
+  'Idoru', '9780425158647', 1
+), (
+  'The State of the Art', '0929480066', 2
+), (
+  'The Simulacra', '9780547572505', 3
+), (
+  'Pattern Recognition', '9780425198681', 1
+), (
+  'A Scanner Darkly', '9780547572178', 3
+);
